1852f655d8b5d3ddaab471521da2c5e1ff9bc7cc
SumayaM <epicgamer50012@gmail.com>
2026-03-25 19:33:46 +0000

    updated updateUser method with PUT only not GET

--- backend/api/views.py
+++ backend/api/views.py
@@ -22,9 +22,19 @@ def getUser(request, pk):
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
-@api_view(['GET', 'PUT'])
-def updateUser():
-    pass
+@api_view(['PUT'])
+def updateUser(request, pk):
+    try:
+        user = User.objects.get(id=pk)
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # ── LOGIN ─────────────────────────────────────────────
 @api_view(['POST'])
